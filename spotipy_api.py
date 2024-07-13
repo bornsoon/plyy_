552eca916fc51f2a38a4640cfbd6c01c62101924
HL <rlimm2@knou.ac.kr>
2024-07-13 16:29:00 +0900

    7/13

--- spotipy_api.py
+++ spotipy_api.py
@@ -1,14 +1,16 @@
-import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import pprint
- 
+import spotipy
+
 cid = '4a07e98721c84fcab2458d5813965796'
 secret = '9fc833bbc4a647c8b56ddb3e952d04b4'
-
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, language='ko')
+def song_sample(title, artist):
 
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, language='ko')
 
-result = sp.search('케이윌 love blossom', limit=1, market='KR')
-pprint.pprint(result)
+    result = sp.search(title + artist, limit=1, market='KR')
+
+    song = {}
+
+    return 

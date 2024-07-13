d33f011987e78543f7e68ee2ca1a3673bc2391b2
HL <rlimm2@knou.ac.kr>
2024-07-13 23:16:11 +0900

    7/13

--- spotipy_api.py
+++ spotipy_api.py
@@ -1,16 +1,33 @@
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
+import pprint
 
 cid = '4a07e98721c84fcab2458d5813965796'
 secret = '9fc833bbc4a647c8b56ddb3e952d04b4'
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, language='ko')
 
-def song_sample(title, artist):
+def track(artist, title):
+    result = sp.search(title + artist, limit=1)
+    rslt = result['tracks']['items'][0]
+    albumTitle = rslt['album']['name']
+    albumImg = [Img for Img in rslt['album']['images'] if Img['height'] == 640][0]['url']
+    artist = rslt['artists'][0]['name']
+    srcTitle = rslt['name']
+    srcId = rslt['id']
 
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager, language='ko')
+    src = {'Id': srcId, 'Title': srcTitle, 'Artist': artist, 'Album': albumTitle, 'AlbumImg': albumImg}
 
-    result = sp.search(title + artist, limit=1, market='KR')
+    return src
 
-    song = {}
 
-    return 
+def spotify_plyy(playlist_id):
+    result = sp.playlist_tracks(playlist_id, additional_types=("track",))
+
+    pprint.pprint(result)
+    return result
+    
+
+if __name__=='__main__':
+    track('케이윌', '체리블라썸')
+    spotify_plyy('3axAvtVlnfhenoPSfOel70')

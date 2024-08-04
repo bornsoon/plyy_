8390fb75007100145747245a3c7dfc1ac9eeca4d
HL <rlimm2@knou.ac.kr>
2024-08-04 23:24:36 +0900

    8/4

--- project1/app2.py
+++ project1/app2.py
@@ -41,148 +41,159 @@ def api_main_tag():
 
 @api_main.route('/plyy')
 def api_main_plyy():
-    query = '''
-            SELECT
-            p.id,
-            p.title,
-            p.img,
-            STRFTIME('%Y-%m-%d', p.gen_date) AS 'generate',
-            STRFTIME('%Y-%m-%d', p.up_date) AS 'update',
-            c.name AS curator,
-            g.name AS genre,
-            COUNT(s.num) AS tracks,
-            SUM(t.rtime) AS times
-            FROM PLYY p
-            JOIN CURATOR c ON p.c_id=c.id
-            JOIN GENRE g ON p.g_id=g.id
-            JOIN SONG s ON p.id=s.p_id
-            JOIN TRACK t ON s.tk_id=t.id
-            GROUP BY p.id;
-            '''
-    plyys = db.get_query(query)
-    result = [dict(row) for row in plyys]
+    try:
+        query = '''
+                SELECT
+                p.id,
+                p.title,
+                p.img,
+                STRFTIME('%Y-%m-%d', p.gen_date) AS 'generate',
+                STRFTIME('%Y-%m-%d', p.up_date) AS 'update',
+                c.name AS curator,
+                g.name AS genre,
+                COUNT(s.num) AS tracks,
+                SUM(t.rtime) AS times
+                FROM PLYY p
+                JOIN CURATOR c ON p.c_id=c.id
+                JOIN GENRE g ON p.g_id=g.id
+                JOIN SONG s ON p.id=s.p_id
+                JOIN TRACK t ON s.tk_id=t.id
+                GROUP BY p.id;
+                '''
+        plyys = db.get_query(query)
+        result = [dict(row) for row in plyys]
 
-    for i in result:
-        tag = db.tag_query('plyy', i['id'], mul=False)
-        if tag:
-            tag = dict(tag)
-            i['tag'] = tag['name']
-        else:
-            i['tag'] = ''
-    
+        for i in result:
+            tag = db.tag_query('plyy', i['id'], mul=False)
+            if tag:
+                tag = dict(tag)
+                i['tag'] = tag['name']
+            else:
+                i['tag'] = ''
+    except:
+        print('플레이리스트 목록을 불러오는데 실패했습니다.')
     
     return jsonify(result)
 
 
 @api_main.route('/curator')
 def api_main_curator():
-    query = '''
-            SELECT
-            id,
-            name,
-            img,
-            intro
-            FROM CURATOR
-            GROUP BY id;
-            '''
-    curators = db.get_query(query)
-    result = [dict(row) for row in curators]
-    
-    for i in result:
-        tags = db.tag_query('curator', i['id'])
-        tag = []
-        for j in tags[:2]:
-            tag.append(j['name'])
-        i['tag'] = tag
+    try:
+        query = '''
+                SELECT
+                id,
+                name,
+                img,
+                intro
+                FROM CURATOR
+                GROUP BY id;
+                '''
+        curators = db.get_query(query)
+        result = [dict(row) for row in curators]
+        
+        for i in result:
+            tags = db.tag_query('curator', i['id'])
+            tag = []
+            for j in tags[:2]:
+                tag.append(j['name'])
+            i['tag'] = tag
 
-    date_query = '''
-                 SELECT
-                 MAX(STRFTIME('%Y-%m-%d', p.gen_date)) AS generate,
-                 MAX(STRFTIME('%Y-%m-%d', p.up_date)) AS 'update'
-                 FROM PLYY p
-                 JOIN CURATOR c ON p.c_id=c.id
-                 GROUP BY c.id
-                 HAVING c.id=?;
-                 '''
-    for i in result:
-        date = db.get_query(date_query, (i['id'],), mul=False)
-        i.update(dict(date))
+        date_query = '''
+                    SELECT
+                    MAX(STRFTIME('%Y-%m-%d', p.gen_date)) AS generate,
+                    MAX(STRFTIME('%Y-%m-%d', p.up_date)) AS 'update'
+                    FROM PLYY p
+                    JOIN CURATOR c ON p.c_id=c.id
+                    GROUP BY c.id
+                    HAVING c.id=?;
+                    '''
+        for i in result:
+            date = db.get_query(date_query, (i['id'],), mul=False)
+            i.update(dict(date))
+    except:
+        print('큐레이터 목록을 불러오는데 실패했습니다.')
 
     return jsonify(result)
 
 
 @api_plyy.route('/<id>')
 def api_plyy_detail(id):
-    plyy_query = '''
-                 SELECT
-                 p.title,
-                 c.name AS curator, 
-                 STRFTIME('%Y-%m-%d', p.gen_date) AS 'generate',
-                 STRFTIME('%Y-%m-%d', p.up_date) AS 'update',
-                 g.name AS genre,
-                 p.cmt AS comment  
-                 FROM PLYY p 
-                 JOIN CURATOR c ON p.c_id=c.id
-                 JOIN GENRE g ON p.g_id=g.id 
-                 WHERE p.id=? GROUP BY p.id;
-                 '''
-    plyy = dict(db.get_query(plyy_query, (id,), mul=False))
+    try:
+        plyy_query = '''
+                    SELECT
+                    p.title,
+                    c.name AS curator, 
+                    STRFTIME('%Y-%m-%d', p.gen_date) AS 'generate',
+                    STRFTIME('%Y-%m-%d', p.up_date) AS 'update',
+                    g.name AS genre,
+                    p.cmt AS comment  
+                    FROM PLYY p 
+                    JOIN CURATOR c ON p.c_id=c.id
+                    JOIN GENRE g ON p.g_id=g.id 
+                    WHERE p.id=? GROUP BY p.id;
+                    '''
+        plyy = dict(db.get_query(plyy_query, (id,), mul=False))
 
-    heart_query = '''
-                  SELECT
-                  COUNT(*) AS heart
-                  FROM p_LIKE
-                  WHERE p_id=?;
-                  '''
-    heart = dict(db.get_query(heart_query, (id,), mul=False))
-    plyy['heart'] = heart['heart']
+        heart_query = '''
+                    SELECT
+                    COUNT(*) AS heart
+                    FROM p_LIKE
+                    WHERE p_id=?;
+                    '''
+        heart = dict(db.get_query(heart_query, (id,), mul=False))
+        plyy['heart'] = heart['heart']
 
-    tracks_query = '''
-                   SELECT t.id,
-                   t.title,
-                   t.img,
-                   t.album,
-                   t.artist,
-                   s.num,
-                   t.rtime
-                   FROM TRACK t 
-                   JOIN SONG s ON t.id=s.tk_id
-                   JOIN PLYY p ON s.p_id=p.id 
-                   WHERE p.id=?;
-                   '''
-    tracks = db.get_query(tracks_query,(id,))
-    tracks = [dict(row) for row in tracks]
+        tracks_query = '''
+                    SELECT t.id,
+                    t.title,
+                    t.img,
+                    t.album,
+                    t.artist,
+                    s.num,
+                    t.rtime
+                    FROM TRACK t 
+                    JOIN SONG s ON t.id=s.tk_id
+                    JOIN PLYY p ON s.p_id=p.id 
+                    WHERE p.id=?;
+                    '''
+        tracks = db.get_query(tracks_query,(id,))
+        tracks = [dict(row) for row in tracks]
 
-    tags = db.tag_query('plyy', id)
-    tags = [dict(row) for row in tags]
+        tags = db.tag_query('plyy', id)
+        tags = [dict(row) for row in tags]
+    except:
+        print('해당 플레이리스트의 상세정보 페이지가 존재하지 않습니다.')
 
     return jsonify({'plyy': plyy, 'tracks': tracks, 'tags': tags})
 
 
 @api_plyy.route('/<id>/<song_num>')
 def api_song(id, song_num):
-    song_query = '''
-                 SELECT 
-                 t.title,
-                 t.artist,
-                 t.img,
-                 t.album,
-                 s.cmt AS comment,
-                 s.vid
-                 FROM TRACK t 
-                 JOIN SONG s ON t.id=s.tk_id 
-                 WHERE s.id=? AND s.num=?
-                 '''
-    song = dict(db.get_query(song_query, (id,song_num), mul=False))
+    try:
+        song_query = '''
+                    SELECT 
+                    t.title,
+                    t.artist,
+                    t.img,
+                    t.album,
+                    s.cmt AS comment,
+                    s.vid
+                    FROM TRACK t 
+                    JOIN SONG s ON t.id=s.tk_id 
+                    WHERE s.id=? AND s.num=?
+                    '''
+        result = dict(db.get_query(song_query, (id,song_num), mul=False))
 
-    total_query = '''
-                  SELECT
-                  COUNT(id) AS total
-                  FROM SONG
-                  WHERE p_id=?
-                  '''
-    total_index = dict(db.get_query(total_query, (id,), mul=False))
+        total_query = '''
+                    SELECT
+                    COUNT(id) AS total
+                    FROM SONG
+                    WHERE p_id=?
+                    '''
+        total_index = dict(db.get_query(total_query, (id,), mul=False))
 
-    song['total_num'] = total_index['total']
+        result['total_num'] = total_index['total']
+    except:
+        print('해당 곡의 상세정보 페이지가 존재하지 않습니다.')
     
-    return jsonify(song)
+    return jsonify(result)
